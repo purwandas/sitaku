7d3440ae6d2a9c55c6c51e9a8ba5c68947f67cda
wo <purwandas@gmail.com>
2020-09-22 14:37:18 +0700

    add autoNumeric plugin to TextInput

--- resources/views/components/form/text_input.blade.php
+++ resources/views/components/form/text_input.blade.php
@@ -6,14 +6,16 @@ $config = FormBuilderHelper::setupDefaultConfig($name, $attributes, true);
 $labelContainerClass = ($config['labelContainerClass'] ?? 'col-md-2').' col-form-label text-right';
 
 if((isset($config['formAlignment']) && $config['formAlignment'] == 'vertical')){
-	$labelContainerClass = 'col-form-label text-right';
+    $labelContainerClass = 'col-form-label text-right';
 }
+
+$config['elOptions']['id'] = $config['elOptions']['id'] ?? 'text-' . $name;
 
 $elOptions = '';
 if(isset($config['elOptions'])){
-	foreach($config['elOptions'] as $attr => $attr_value){
-		$elOptions .= $attr . "='" . trim($attr_value) . "' ";
-	}
+    foreach($config['elOptions'] as $attr => $attr_value){
+        $elOptions .= $attr . "='" . trim($attr_value) . "' ";
+    }
 }
 
 @endphp
@@ -27,7 +29,13 @@ if(isset($config['elOptions'])){
     @endif
 
     {{-- Input here --}}
-    <input name="{{$name}}" value="{{$value}}" class="form-control" type="text" {!! $elOptions !!}>
+    @if(@$config['autoNumeric'])
+        <input type="text" id="{{$config['elOptions']['id']}}1" class="form-control" type="text" {!! $elOptions !!}>
+        <input name="{{$name}}" value="{{$value}}" class="form-control" type="hidden" {!! $elOptions !!}>
+    @else
+        <input name="{{$name}}" value="{{$value}}" class="form-control" type="text" {!! $elOptions !!}>
+    @endif
+
     @error($name)
         <div style="margin-top: 5px;" class="alert alert-danger alert-dismissible fade show" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -43,3 +51,19 @@ if(isset($config['elOptions'])){
         @endif
     @endif
 </div>
+
+@if(@$config['autoNumeric'])
+@section('auto-numeric-plugin-js')
+<script src="{{asset('assets/formbuilder/auto-numeric/autoNumeric.js')}}"></script>
+<script type="text/javascript">
+    @if( is_array($config['autoNumeric']) )
+        $('#{{$config['elOptions']['id']}}1').autoNumeric('init',{!! $config['autoNumeric'] !!});
+    @else
+        $('#{{$config['elOptions']['id']}}1').autoNumeric('init');
+    @endif
+    $("#{{$config['elOptions']['id']}}1").change(function(){
+        $("#{{$config['elOptions']['id']}}").val($(this).autoNumeric('get'));
+    });
+</script>
+@endsection
+@endif
